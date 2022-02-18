5551b1b878aa9b0e58a2d052789bd24d0d0f7008
Michael Hall <michael@mbh.sh>
2022-02-18 14:42:38 +1000

    use dir instead of json

--- workflow/scripts/xpore_config.py
+++ workflow/scripts/xpore_config.py
@@ -9,7 +9,7 @@ import yaml
 data = defaultdict(dict)
 for p in map(Path, snakemake.input.jsons):
     cond, rep = p.parent.name.split("_")
-    data[cond][rep] = str(p.resolve())
+    data[cond][rep] = str(p.resolve().parent)
 
 criteria = {
     "readcount_min": snakemake.params.readcount_min,
